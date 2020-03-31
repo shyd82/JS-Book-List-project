e8ff6f682a20109d407786e8609e64b32a91ad95
shyd82 <shyd82@hotmail.com>
2020-03-31 13:49:20 -0400

    Book Add project 03-31

--- app.js
+++ app.js
@@ -31,6 +31,40 @@ UI.prototype.addBookToList = function(book){
 };
 
 
+// Show Alert     以下不懂？？？？？？？？
+
+UI.prototype.showAlert = function(message, className){
+    // create the div element:    
+    const div = document.createElement('div');
+    // Add classes:
+    div.className = `alert ${className}`;
+    // Add text node
+    div.appendChild(document.createTextNode(message));
+    // Get the parent:
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    // Insert the Alert
+    container.insertBefore(div,form);
+
+    // Timeout after 3 sec
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+        },3000);
+
+}
+
+
+
+// Delete book
+
+UI.prototype.deletebook = function(target){
+    if (target.className === 'delete'){
+        target.parentElement.parentElement.remove();
+    }
+}
+
+
+
 // Clear Fields
 UI.prototype.clearFields = function(){
     document.getElementById('title').value = '';
@@ -63,21 +97,38 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     // Instantiating the UI:
     const ui = new UI;
 
+    if (title === '' || author === '' || isbn === ''){
+        ui.showAlert('Please fill in all fields','error')
+
+    }else {
+        
+        // 下面两行移动上来到这里：
+        ui.addBookToList(book);
+        ui.clearFields()
+
+        // Show success message:
+        ui.showAlert('Book Added!', 'success');
+
+
+
+    }
+    ;
 
 
 
 
-    // 接下来，we are going to add the book object down  here and the UI object is going to take care of that
 
-    // 也就是说当summit提交后，事件触发，需要将上一步的book放在最下面的table里，这个过程由UI来完成
+    // // 接下来，we are going to add the book object down  here and the UI object is going to take care of that
+
+    // // 也就是说当summit提交后，事件触发，需要将上一步的book放在最下面的table里，这个过程由UI来完成
 
 
 
-    // Add book to list:
-    ui.addBookToList(book);
+    // // Add book to list:
+    // ui.addBookToList(book);
 
-    // Clear fields
-    ui.clearFields()
+    // // Clear fields
+    // ui.clearFields()
 
 
 
@@ -86,4 +137,23 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     e.preventDefault();
 })
 
+
+// Event Listener for delete：
+document.getElementById('book-list').addEventListener('click',function(e){
+
+    // Instantiating the UI:
+    const ui = new UI;
+
+    // Delete book
+    ui.deletebook(e.target);
+
+
+    // Show message:
+    ui.showAlert('Book Removed!', 'success');
+
+
+
+
+    e.preventDefault();
+});
 
